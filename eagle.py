fee1ae43d65e007d89c0767e465f7a8707884d23
Tony Kuo <kuo@debianServer>
2016-04-27 09:30:39 +0900

    clean up

--- eagle.py
+++ eagle.py
@@ -307,7 +307,7 @@ def evaluateVariant(fn, varid, var_set):
                 altcount[currentset] = 0;
 
             refprior = 0.5;
-            if multivariant or len(currentset) == 1: altprior = np.log((1-refprior) / float(2)); # one hypothesis, either one variant or multiple as a haplotype, homozygous & non-homozygous
+            if multivariant or len(currentset) == 1: altprior = np.log((1-refprior) / float(2)); # one hypothesis, either one variant or multiple variants as a haplotype, homozygous & non-homozygous
             else: altprior = np.log((1-refprior) / float(len(var_set)*2)); # remainder divided evenly among the variant hypotheses, homozygous & non-homozygous
             refprior = np.log(refprior);
 
@@ -336,7 +336,7 @@ def evaluateVariant(fn, varid, var_set):
                 if debug: print('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}'.format(prgu, prgv, pelsewhere, varid[0], currentset, readid, altcount[currentset])); # ln likelihoods
             if debug: print('-=-\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(ref, het[currentset], alt[currentset], varid[0], currentset, altcount[currentset])); # ln likelihoods
 
-        if len(var_set) > 1: total = logsumexp( [ref+np.log(2)] + list(alt.values()) + list(het.values()) ); # Double reference probability in total if there are variant sets hypotheses
+        if not multivariant and len(var_set) > 1: total = logsumexp( [ref+np.log(2)] + list(alt.values()) + list(het.values()) ); # Double reference probability in total if marginal probabilities
         else: total = logsumexp( [ref] + list(alt.values()) + list(het.values()) );
         for i in var_set:
             marginal_alt = [];
@@ -347,7 +347,6 @@ def evaluateVariant(fn, varid, var_set):
             if multivariant: outstr = '{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t'.format(varid[0], i[0], i[1], i[2], altcount[currentset]+refcount[currentset], altcount[currentset]);
             else: outstr = '{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t'.format(varid[0], i[0], i[1], i[2], max(altcount.values())+max(refcount.values()), max(altcount.values()));
             # Probability and odds in log10
-            #outstr += '{0}\t{1}\t'.format((logsumexp(marginal_alt) - total) / np.log(10), (logsumexp(marginal_alt) - logsumexp(not_alt)) / np.log(10)); 
             outstr += '{0}\t{1}\t'.format((logsumexp(marginal_alt) - total) / np.log(10), (max(marginal_alt) - max(not_alt)) / np.log(10)); 
             # Print the variant set entries if exists    
             if len(var_set) > 1: outstr += '{0}'.format(var_set);

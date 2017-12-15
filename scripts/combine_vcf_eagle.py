2391716c0a2d57189da97892fcb73287bb788044
adrift <kuo.tony@aist.go.jp>
2017-12-15 11:46:57 +0900

    added more utility scripts

---
+++ scripts/combine_vcf_eagle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+# Copyright 2016 Tony Kuo
+# This program is distributed under the terms of the GNU General Public License
+
+# Utility Script
+# Compare results of EAGLE to VCF truth data
+# True positives are written to stdout
+# False positives are written to file
+
+from __future__ import print_function
+import argparse, re, sys, os;
+from signal import signal, SIGPIPE, SIG_DFL;
+signal(SIGPIPE,SIG_DFL) 
+
+def naturalSort(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)] 
+    return sorted(l, key=alphanum_key)
+
+def readEAGLE(filename):
+    entry = {};
+    with open(filename, 'r') as fh:
+        for line in fh:
+            if re.match('^#', line): continue;
+            t = line.strip().split('\t');
+            entry[ '\t'.join(t[0:4]) ] = t;
+    fh.close;
+    return(entry);
+
+def readVCF(filename, eagle, raw):
+    entry = {};
+    with open(filename, 'r') as fh:
+        for line in fh:
+            if re.match('^#', line): 
+                print(line);
+                continue;
+
+            t = line.strip().split('\t');
+            key = "{}\t{}\t{}\t{}".format(t[0], t[1], t[3], t[4]);
+            print(key)
+            if key in eagle:
+                if raw:
+                    af = float(eagle[key][6]) / float(eagle[key][4]);
+                    lr = float(eagle[key][8]);
+                else:
+                    af = float(eagle[key][5]);
+                    lr = float(eagle[key][6]);
+                t[7] += ";EAGLEAF={:.2f};EAGLELR={:.4f}".format(af, lr);
+                print('\t'.join(t))
+
+    fh.close;
+    return(entry);
+
+def main():
+    parser = argparse.ArgumentParser(description='Compare to VCF Truth Data');
+    parser.add_argument('-v', help='VCF file');
+    parser.add_argument('-e', help='EAGLE file');
+    parser.add_argument('-raw', action='store_true', help='raw EAGLE output, otherwise output after compile_likelihood');
+    args = parser.parse_args();
+
+    eagle = readEAGLE(args.e); 
+    readVCF(args.v, eagle, args.raw); 
+
+if __name__ == '__main__':
+    try:
+        main();
+    except KeyboardInterrupt: 
+        try:
+            sys.exit(0);
+        except SystemExit:
+            os._exit(0);
---
+++ scripts/example.sh
@@ -0,0 +1,78 @@
+#! /bin/bash
+## Example workflows using EAGLE
+
+CPU=8
+
+## -- Germline Variant Calling --
+# Directly after variant calling, run eagle to calculate variant log likelihood ratios
+eagle -t $CPU -v variants.vcf -a align.bam -r reference.fa > eagle.txt
+
+# Filter variants based on eagle output
+python scripts/compile_likelihoods.py -p eagle.txt -minlr 5 -minaf 0.3 -mindepth 10 -seen > eagle.filter.txt
+
+# Filter the VCF and add eagle score as an annotation
+python scripts/combine_vcf_eagle.py -v variants.vcf -e eagle.filter.txt > variants.eagle.filter.vcf
+
+## -- Somatic Variant Calling --
+# Run variant calling on tumor samples, 
+# then run eagle to calculate log likelihood ratios for both normal and tumor samples
+eagle -t $CPU -v tumor.vcf -a tumor.bam -r reference.fa > tumor.txt
+eagle -t $CPU -v tumor.vcf -a normal.bam -r reference.fa > normal.txt
+
+# Filter variants based on eagle outputs, where positive sample is tumor and negative sample is normal
+python scripts/compile_likelihoods.py -p tumor.txt -n normal.txt -minlr 5 -minaf 0.05 -maxlr -2 -maxaf 0.04  -mindepth 10 -seen > somatic.filter.txt
+
+# Filter the VCF and add eagle score as an annotation
+python scripts/combine_vcf_eagle.py -v tumor.vcf -e somatic.filter.txt > variants.somatic.vcf
+
+## -- Trio De Novo Variant Calling --
+# Run variant calling on each of child and parents separately, then make a union vcf
+python ~/scripts/tablize.py -i 0-4 child.vcf father.vcf mother.vcf | cut -f 1-5 > union.vcf
+# Add headers to union.vcf [optional]
+grep '^#' child.vcf | cat - union.vcf > tmp && mv tmp union.vcf
+
+# then run eagle to calculate log likelihood ratios for both all samples using union.vcf
+eagle -t $CPU -v union.vcf -a child.bam -r reference.fa > child.txt
+eagle -t $CPU -v union.vcf -a father.bam -r reference.fa > father.txt
+eagle -t $CPU -v union.vcf -a mother.bam -r reference.fa > mother.txt
+
+# Filter variants based on eagle outputs, where positive sample is child and negative sample is parent
+python scripts/compile_likelihoods.py -p child.txt -n father.txt -minlr 5 -minaf 0.05 -maxlr -2 -maxaf 0.04  -mindepth 10 -seen > child.vs.father.txt
+python scripts/compile_likelihoods.py -p child.txt -n mother.txt -minlr 5 -minaf 0.05 -maxlr -2 -maxaf 0.04  -mindepth 10 -seen > child.vs.mother.txt
+
+# Get the variants which are present only in the child
+python scripts/tablize.py -a -i 0-7 child.vs.father.txt child.vs.mother.txt > child.vs.parents.txt
+
+# Filter the VCF and add eagle score as an annotation
+python scripts/combine_vcf_eagle.py -v child.vcf -e child.vs.parents.txt > child.denovo.vcf
+
+## -- Read Classification for RNA-Seq expression quantification with homeologs --
+## Requires LAST (http://last.cbrc.jp/)
+## Align the sample data to each origin specific reference separately
+
+## From origin specific transcripts in an allopolyploid, find homeologs and their genotype differences
+lastdb -uNEAR -R01 A_origin A_transcripts.fa
+lastdb -uNEAR -R01 B_origin B_transcripts.fa
+
+lastal -D10000000000 A_origin -P$CPU B_transcripts.fa | last-map-probs -m 0.49 > A_origin.maf
+lastal -D10000000000 B_origin -P$CPU A_transcripts.fa | last-map-probs -m 0.49 > B_origin.maf
+
+# [Note] check to make sure your transcripts are based on exons or CDS in the annotation. If CDS then change below, -f exon to -f CDS
+python scripts/homeolog_genotypes.py -f exon -o Ref_A -g annotation_A.gtf A_origin.maf B_origin.maf
+python scripts/homeolog_genotypes.py -f exon -o Ref_B -g annotation_B.gtf B_origin.maf A_origin.maf
+
+eagle -t $CPU -a data_align2_A.bam -r A.reference.fa -v Ref_A.dna.vcf --omega=1e-40 --mvh --splice --isc --verbose 1> Ref_A.sample.txt 2> Ref_A.sample.readinfo.txt
+eagle-rc -a data_align2_A.bam --listonly -o Ref_A.sample Ref_A.sample.txt Ref_A.sample.readinfo.txt > Ref_A.sample.list
+
+eagle -t $CPU -a data_align2_B.bam -r B.reference.fa -v Ref_B.dna.vcf --omega=1e-40 --mvh --splice --isc --verbose 1> Ref_B.sample.txt 2> Ref_B.sample.readinfo.txt
+eagle-rc -a data_align2_B.bam --listonly -o Ref_B.sample Ref_B.sample.txt Ref_B.sample.readinfo.txt > Ref_B.sample.list
+
+## Find the consensus classification based on likelihood
+python scripts/mirror_consensus.py Ref_A.sample.list Ref_B.sample.list > sample.consensus.list
+
+## Write bam files based on consensus list, using A as the reference
+eagle-rc -a data_align2_A.bam -o sample --readlist sample.consensus.list
+
+## Perform read counting as you prefer, for example:
+featureCounts -T $CPU -t exon -g transcript_id -a annotation_A.gtf -o sample.A.counts.txt sample.ref.bam
+featureCounts -T $CPU -t exon -g transcript_id -a annotation_A.gtf -o sample.B.counts.txt sample.alt.bam

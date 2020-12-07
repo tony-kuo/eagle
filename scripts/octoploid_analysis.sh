c4dcb465e477bbb972d34ec634b1fd1bad4e8bbf
tony-kuo <tony.cy.kuo@gmail.com>
2020-12-07 10:00:56 -0500

    octoploid scripts

---
+++ scripts/octoploid_analysis.sh
@@ -0,0 +1,164 @@
+#! /bin/bash
+# Example workflow using EAGLE-RC for octoploid
+# LAST at: http://last.cbrc.jp/ 
+# STAR at: https://github.com/alexdobin/STAR
+# featureCounts at: http://bioinf.wehi.edu.au/featureCounts/
+
+## Homeolog identification
+
+REF=Taes_genome_2017_05
+GTF=iwgsc_refseqv1.0_HighConf_UTR_2017May05
+CPU=8
+
+gffread -T -o refseq.gtf $GTF.gff3 # gff to gtf
+gffread -g $REF.fa -w refseq.fa $GTF.gff3
+
+# Assuming we have the genome assemblies and annotation for the A, B, D subgenomes
+grep '^chr.A' refseq.gtf > refseq.chrA.gtf
+grep '^chr.B' refseq.gtf > refseq.chrB.gtf
+grep '^chr.D' refseq.gtf > refseq.chrD.gtf
+
+gffread -g $REF.fa -w chrA.exon.fa refseq.chrA.gtf
+gffread -g $REF.fa -w chrB.exon.fa refseq.chrB.gtf
+gffread -g $REF.fa -w chrD.exon.fa refseq.chrD.gtf
+
+# **IMPORTANT**
+# Any other transcriptomes should be in similar format
+# i.e. chrC.exon.fa
+
+
+# Reciprocal best hit, assuming you have exon sequences
+lastdb -uNEAR -R01 chrA_db chrA.exon.fa
+lastdb -uNEAR -R01 chrB_db chrB.exon.fa
+lastdb -uNEAR -R01 chrC_db chrC.exon.fa
+lastdb -uNEAR -R01 chrD_db chrD.exon.fa
+
+lastal chrA_db -P$CPU -D10000000000 chrB.exon.fa | last-map-probs -m 0.49 > A.B.maf
+lastal chrA_db -P$CPU -D10000000000 chrC.exon.fa | last-map-probs -m 0.49 > A.C.maf
+lastal chrA_db -P$CPU -D10000000000 chrD.exon.fa | last-map-probs -m 0.49 > A.D.maf
+
+lastal chrB_db -P$CPU -D10000000000 chrA.exon.fa | last-map-probs -m 0.49 > B.A.maf
+lastal chrB_db -P$CPU -D10000000000 chrC.exon.fa | last-map-probs -m 0.49 > B.C.maf
+lastal chrB_db -P$CPU -D10000000000 chrD.exon.fa | last-map-probs -m 0.49 > B.D.maf
+
+lastal chrC_db -P$CPU -D10000000000 chrA.exon.fa | last-map-probs -m 0.49 > C.A.maf
+lastal chrC_db -P$CPU -D10000000000 chrB.exon.fa | last-map-probs -m 0.49 > C.B.maf
+lastal chrC_db -P$CPU -D10000000000 chrD.exon.fa | last-map-probs -m 0.49 > C.D.maf
+
+lastal chrD_db -P$CPU -D10000000000 chrA.exon.fa | last-map-probs -m 0.49 > D.A.maf
+lastal chrD_db -P$CPU -D10000000000 chrB.exon.fa | last-map-probs -m 0.49 > D.B.maf
+lastal chrD_db -P$CPU -D10000000000 chrC.exon.fa | last-map-probs -m 0.49 > D.C.maf
+
+# Create VCFs based on genotype differences between homeologs
+python scripts/homeolog_genotypes.py -o A.vs.B -f exon -g refseq.gtf A.B.maf B.A.maf # coordinates based on A
+python scripts/homeolog_genotypes.py -o A.vs.C -f exon -g refseq.gtf A.C.maf C.A.maf # coordinates based on A
+python scripts/homeolog_genotypes.py -o A.vs.D -f exon -g refseq.gtf A.D.maf D.A.maf # coordinates based on A
+
+python scripts/homeolog_genotypes.py -o B.vs.A -f exon -g refseq.gtf B.A.maf A.B.maf # coordinates based on B
+python scripts/homeolog_genotypes.py -o B.vs.C -f exon -g refseq.gtf B.C.maf C.B.maf # coordinates based on B
+python scripts/homeolog_genotypes.py -o B.vs.D -f exon -g refseq.gtf B.D.maf D.B.maf # coordinates based on B
+
+python scripts/homeolog_genotypes.py -o C.vs.A -f exon -g refseq.gtf C.A.maf A.C.maf # coordinates based on C, will not have valid gtf.vcf as C not in refseq.gtf
+python scripts/homeolog_genotypes.py -o C.vs.B -f exon -g refseq.gtf C.B.maf B.C.maf # coordinates based on C, will not have valid gtf.vcf as C not in refseq.gtf
+python scripts/homeolog_genotypes.py -o C.vs.D -f exon -g refseq.gtf C.D.maf D.C.maf # coordinates based on C, will not have valid gtf.vcf as C not in refseq.gtf
+
+python scripts/homeolog_genotypes.py -o D.vs.A -f exon -g refseq.gtf D.A.maf A.D.maf # coordinates based on D
+python scripts/homeolog_genotypes.py -o D.vs.B -f exon -g refseq.gtf D.B.maf B.D.maf # coordinates based on D
+python scripts/homeolog_genotypes.py -o D.vs.C -f exon -g refseq.gtf D.C.maf C.D.maf # coordinates based on D
+
+# Quad copy homeologs
+perl quad_homeolog.pl A.vs.B.reciprocal_best A.vs.C.reciprocal_best A.vs.D.reciprocal_best B.vs.C.reciprocal_best B.vs.D.reciprocal_best C.vs.D.reciprocal_best > homeolog.ABCD.list
+
+# Triple copy homeologs
+perl triple_homeolog.pl A.vs.B.reciprocal_best A.vs.C.reciprocal_best B.vs.C.reciprocal_best > homeolog.ABC.list
+perl triple_homeolog.pl A.vs.B.reciprocal_best A.vs.D.reciprocal_best B.vs.D.reciprocal_best > homeolog.ABD.list
+perl triple_homeolog.pl A.vs.C.reciprocal_best A.vs.D.reciprocal_best C.vs.D.reciprocal_best > homeolog.ACD.list
+perl triple_homeolog.pl B.vs.C.reciprocal_best B.vs.D.reciprocal_best C.vs.D.reciprocal_best > homeolog.BCD.list
+
+
+## Origin specific alignment with STAR for RNA-seq to Genome alignment
+#GENDIR=/project/wheat/stargenome
+#CHR="chrA chrB chrD"
+#for n in $CHR; do
+#    mkdir -p $GENDIR\_$n
+#    STAR --runMode genomeGenerate --genomeDir $GENDIR\_$n --genomeFastaFiles $REF.$n.fa --sjdbGTFfile $GTF.gtf --runThreadN $CPU
+#
+#    for i in `ls *_R1.fastq.gz`; do 
+#        F=`basename $i _R1.fastq.gz`
+#        mkdir -p ./$n/star_$F
+#        STAR --genomeDir $GENDIR\_$n --readFilesCommand zcat --readFilesIn $F\_R1.fastq.gz $F\_R2.fastq.gz \
+#            --outFileNamePrefix star_$F- --runThreadN $CPU --genomeLoad NoSharedMemory \
+#            --outSAMstrandField intronMotif --outFilterIntronMotifs RemoveNoncanonicalUnannotated \
+#            --outSJfilterCountUniqueMin 3 2 2 2 --outMultimapperOrder Random --outFilterType BySJout \
+#            --outStd SAM | samtools view -Shb - > ./$n/$F.bam
+#        samtools sort -o ./$n/$F.refsort.bam ./$n/$F.bam 
+#        samtools index -c ./$n/$F.refsort.bam
+#        mv star_$F-* ./$n/star_$F
+#    done
+#done
+
+## Origin specific alignment with Bowtie2 for RNA-seq to Transcriptome alignment
+GENDIR=/project/wheat/bowtie2
+CHR="chrA chrB chrD chrC"
+for n in $CHR; do
+    mkdir -p $GENDIR
+    bowtie2-build -f $CHR.exon.fa $GENDIR/$n --threads $CPU
+
+    for i in `ls *_R1.fastq.gz`; do 
+        F=`basename $i _R1.fastq.gz`
+        mkdir -p ./$n/bowtie2_$F
+        bowtie2 -x $GENDIR/$n -1 $F\_R1.fastq.gz -2 $F\_R2.fastq.gz --threads $CPU | samtools view -SbF4 - > ./$n/$F.bam
+        samtools sort -o ./$n/$F.refsort.bam ./$n/$F.bam 
+        samtools index -c ./$n/$F.refsort.bam
+    done
+done
+
+## EAGLE-RC: read classification and the quantification with featureCounts
+# Put the appropriate vcfs to the corresponding dir, i.e. A.vs.*.gtf.vcf in chrA
+#CHR="chrA chrB chrD"
+#for n in $CHR; do
+#    cd $n
+#    for i in `ls *.refsort.bam`; do 
+#        F=`basename $i .refsort.bam`
+#        for j in `ls *.gtf.vcf`; do
+#            V=`basename $j .gtf.vcf`
+#            eagle -t 8 -a $F.refsort.bam -r ../$REF.$n.fa -v $V.gtf.vcf --splice --rc 1> $F.$V.txt 2> $F.$V.readinfo.txt
+#            eagle-rc --listonly -a $F.refsort.bam -o $F.$V -v $F.$V.txt $F.$V.readinfo.txt > $F.$V.list
+#        done
+#    done
+#    cd ..
+#done
+
+# For transcriptome alignments
+CHR="chrA chrB chrD chrC"
+for n in $CHR; do
+    cd $n
+    for i in `ls *.refsort.bam`; do 
+        F=`basename $i .refsort.bam`
+        for j in `ls *.gtf.vcf`; do
+            V=`basename $j .gtf.vcf`
+            eagle -t 8 -a $F.refsort.bam -r ../$CHR.exon.fa -v $V.raw.vcf --rc 1> $F.$V.txt 2> $F.$V.readinfo.txt
+            eagle-rc --listonly -a $F.refsort.bam -o $F.$V -v $F.$V.txt $F.$V.readinfo.txt > $F.$V.list
+        done
+    done
+    cd ..
+done
+
+
+mkdir -p eagle
+for i in `ls *_R1.fastq.gz`; do
+    F=`basename $i _R1.fastq.gz`
+    python scripts/ref4_consensus.py --pe -u -d -o eagle/$F.ref \
+        -A chrA/$F.A.vs.B.list chrA/$F.A.vs.C.list chrA/$F.A.vs.D.list \
+        -B chrB/$F.B.vs.A.list chrB/$F.B.vs.C.list chrB/$F.B.vs.D.list \
+        -C chrC/$F.C.vs.A.list chrC/$F.C.vs.B.list chrC/$F.C.vs.D.list \
+        -D chrD/$F.D.vs.A.list chrD/$F.D.vs.B.list chrD/$F.D.vs.C.list
+    eagle-rc --refonly --readlist -a chrA/$F.refsort.bam -o eagle/$F.chrA eagle/$F.ref.chrA.list
+    eagle-rc --refonly --readlist -a chrB/$F.refsort.bam -o eagle/$F.chrB eagle/$F.ref.chrB.list
+    eagle-rc --refonly --readlist -a chrC/$F.refsort.bam -o eagle/$F.chrC eagle/$F.ref.chrC.list
+    eagle-rc --refonly --readlist -a chrD/$F.refsort.bam -o eagle/$F.chrD eagle/$F.ref.chrD.list
+#    featureCounts -T 8 -t exon -g transcript_id -a refseq.chrA.gtf -o eagle/$F.chrA.counts.txt eagle/$F.chrA.ref.bam
+#    featureCounts -T 8 -t exon -g transcript_id -a refseq.chrB.gtf -o eagle/$F.chrB.counts.txt eagle/$F.chrB.ref.bam
+#    featureCounts -T 8 -t exon -g transcript_id -a refseq.chrC.gtf -o eagle/$F.chrC.counts.txt eagle/$F.chrC.ref.bam
+#    featureCounts -T 8 -t exon -g transcript_id -a refseq.chrD.gtf -o eagle/$F.chrD.counts.txt eagle/$F.chrD.ref.bam
+done
---
+++ scripts/ref4_consensus.py
@@ -0,0 +1,203 @@
+#!/usr/bin/python
+
+# Copyright 2017 Tony Kuo
+# This program is distributed under the terms of the GNU General Public License
+
+# Utility Script
+# Compiles a consensus classification based on the results of readclassify, for octoploid.
+# Classification is determined by the reference with the highest probability.
+
+from __future__ import print_function
+import argparse
+import re, os, sys
+import numpy as np
+from scipy.special import logsumexp
+from datetime import datetime
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE,SIG_DFL) 
+
+def readFile(fn, entry):
+    with open(fn, 'r') as fh:
+        for line in fh:
+            if re.match('^#', line): continue
+
+            t = line.strip().split('\t')
+            if len(t) < 8:
+                t.append(' ')
+
+            key = '{}\t{}'.format(t[0], t[7])
+            pos = '{}\t{}'.format(t[2], t[3])
+
+            if key not in entry: # pos, prgu, total, pout, n
+                entry[key] = (pos, float(t[4]), float(t[5]), float(t[6]), 0)
+            elif key in entry:
+                entry[key] = (pos, entry[key][1], np.logaddexp(entry[key][2], float(t[5])), entry[key][3], entry[key][4] + 1)
+    fh.close
+    print('Read:\t{}\t{}'.format(fn, datetime.now()), file=sys.stderr)
+    return(entry)
+
+def combinePE(data):
+    entry = {}
+    for key in data:
+        t = key.strip().split('\t')
+        if t[0] not in entry:
+            entry[t[0]] = data[key]
+        elif t[0] in entry:
+            entry[t[0]] = (entry[t[0]][0], entry[t[0]][1] + data[key][1], entry[t[0]][2] + data[key][2], entry[t[0]][3] + data[key][3], max(entry[t[0]][4], data[key][4]))
+    return(entry)
+
+def classifySingle(key, chrA, fh, p_threshold):
+    if chrA[key][4] > 0 and chrA[key][1] - chrA[key][2] >= p_threshold: c = 'REF'
+    else: c = 'UNK'
+    t = key.strip().split('\t')
+    if len(t) > 1: f = t[1]
+    else: f = '-'
+    print('{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(t[0], c, chrA[key][0], chrA[key][1], chrA[key][2], chrA[key][3], f), file=fh)
+
+def classifyDouble(key, chrA, chrB, idx, fh, p_threshold, m_threshold):
+    pos = [chrA[key][0], chrB[key][0]]
+    x = [chrA[key][1], chrB[key][1]]
+    y = [chrA[key][2], chrB[key][2]]
+    z = [chrA[key][3], chrB[key][3]] 
+    n = [chrA[key][4], chrB[key][4]] # number of pair-wise analysis that cross variants
+
+    p = [x[i] - y[i] for i in range(len(x))]
+    total = logsumexp(p)
+    m = [p[i] - total for i in range(len(p))]
+    i = max(range(len(p)), key=p.__getitem__)
+
+    if n[i] > 1 and p[i] >= p_threshold and m[i] >= m_threshold: c = 'REF'
+    else: c = 'UNK'
+    t = key.strip().split('\t')
+    if len(t) > 1: f = t[1]
+    else: f = '-'
+    print('{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(t[0], c, pos[i], x[i], y[i], z[i], f), file=fh[idx[i]])
+
+def classifyTriple(key, chrA, chrB, chrC, idx, fh, p_threshold, m_threshold):
+    pos = [chrA[key][0], chrB[key][0], chrC[key][0]]
+    x = [chrA[key][1], chrB[key][1], chrC[key][1]]
+    y = [chrA[key][2], chrB[key][2], chrC[key][2]]
+    z = [chrA[key][3], chrB[key][3], chrC[key][3]]
+    n = [chrA[key][4], chrB[key][4], chrC[key][4]] # number of pair-wise analysis that cross variants
+
+    p = [x[i] - y[i] for i in range(len(x))]
+    total = logsumexp(p)
+    m = [p[i] - total for i in range(len(p))]
+    i = max(range(len(p)), key=p.__getitem__)
+
+    if n[i] > 1 and p[i] >= p_threshold and m[i] >= m_threshold: c = 'REF'
+    else: c = 'UNK'
+    t = key.strip().split('\t')
+    if len(t) > 1: f = t[1]
+    else: f = '-'
+    print('{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(t[0], c, pos[i], x[i], y[i], z[i], f), file=fh[idx[i]])
+
+def writeTable(chrA, chrB, chrC, chrD, doubles, unique_reads, out_prefix):
+    fhA = open(out_prefix + '.chrA.list', 'w')
+    fhB = open(out_prefix + '.chrB.list', 'w')
+    fhC = open(out_prefix + '.chrC.list', 'w')
+    fhD = open(out_prefix + '.chrD.list', 'w')
+    fh = [fhA, fhB, fhC, fhD]
+
+    p_threshold = np.log(2)
+    m_threshold = np.log(0.51)
+    for key in chrA:
+        if key not in chrB or key not in chrC or key not in chrD: continue
+
+        pos = [chrA[key][0], chrB[key][0], chrC[key][0], chrD[key][0]]
+        x = [chrA[key][1], chrB[key][1], chrC[key][1], chrD[key][1]]
+        y = [chrA[key][2], chrB[key][2], chrC[key][2], chrD[key][2]]
+        z = [chrA[key][3], chrB[key][3], chrC[key][3], chrD[key][3]]
+        n = [chrA[key][4], chrB[key][4], chrC[key][4], chrD[key][4]] # number of pair-wise analysis that cross variants
+
+        p = [x[i] - y[i] for i in range(len(x))]
+        total = logsumexp(p)
+        m = [p[i] - total for i in range(len(p))]
+        i = max(range(len(p)), key=p.__getitem__)
+
+        if n[i] > 1 and p[i] >= p_threshold and m[i] >= m_threshold: c = 'REF'
+        else: c = 'UNK'
+        t = key.strip().split('\t')
+        if len(t) > 1: f = t[1]
+        else: f = '-'
+        print('{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(t[0], c, pos[i], x[i], y[i], z[i], f), file=fh[i])
+
+    if triples:
+        for key in chrA:
+            if key in chrB and key in chrC and key not in chrD: classifyTriple(key, chrA, chrB, chrC, (0, 1, 2), fh, p_threshold, m_threshold)
+            if key in chrB and key not in chrC and key in chrD: classifyTriple(key, chrA, chrB, chrD, (0, 1, 3), fh, p_threshold, m_threshold)
+            if key not in chrB and key in chrC and key in chrD: classifyTriple(key, chrA, chrC, chrD, (0, 2, 3), fh, p_threshold, m_threshold)
+        for key in chrB:
+            if key not in chrA and key in chrC and key in chrD: classifyTriple(key, chrB, chrC, chrD, (1, 2, 3), fh, p_threshold, m_threshold)
+
+    if doubles:
+        for key in chrA:
+            if key in chrB and key not in chrC and key not in chrD: classifyDouble(key, chrA, chrB, (0, 1), fh, p_threshold, m_threshold)
+            elif key not in chrB and key in chrC and key not in chrD: classifyDouble(key, chrA, chrD, (0, 2), fh, p_threshold, m_threshold)
+            elif key not in chrB and key not in chrC and key in chrD: classifyDouble(key, chrA, chrD, (0, 3), fh, p_threshold, m_threshold)
+        for key in chrB:
+            if key not in chrA and key in chrC and key not in chrD: classifyDouble(key, chrB, chrD, (1, 2), fh, p_threshold, m_threshold)
+            elif key not in chrA and key not in chrC and key in chrD: classifyDouble(key, chrB, chrD, (1, 3), fh, p_threshold, m_threshold)
+        for key in chrC:
+            if key not in chrA and key not in chrB and key in chrD: classifyDouble(key, chrB, chrD, (2, 3), fh, p_threshold, m_threshold)
+
+    if unique_reads:
+        for key in chrA:
+            if key not in chrB and key not in chrC and key not in chrD: classifySingle(key, chrA, fhA, p_threshold)
+        for key in chrB:
+            if key not in chrA and key not in chrC and key not in chrD: classifySingle(key, chrB, fhB, p_threshold)
+        for key in chrC:
+            if key not in chrA and key not in chrB and key not in chrD: classifySingle(key, chrB, fhB, p_threshold)
+        for key in chrD:
+            if key not in chrA and key not in chrB and key not in chrC: classifySingle(key, chrD, fhD, p_threshold)
+
+    fhA.close()
+    fhB.close()
+    fhC.close()
+    fhD.close()
+    print('Done:\t{}'.format(datetime.now()), file=sys.stderr)
+
+def main():
+    parser = argparse.ArgumentParser(description='Determine read classification REF = A, B, D.')
+    parser.add_argument('-A', nargs='+', required=True, help='3 list files: from readclassify with A as reference')
+    parser.add_argument('-B', nargs='+', required=True, help='3 list files: from readclassify with B as reference')
+    parser.add_argument('-C', nargs='+', required=True, help='3 list files: from readclassify with C as reference')
+    parser.add_argument('-D', nargs='+', required=True, help='3 list files: from readclassify with D as reference')
+    parser.add_argument('-o', type=str, required=True, help='output file prefix')
+    parser.add_argument('-u', action='store_true', help='include reads that map uniquely to one subgenome')
+    parser.add_argument('-d', action='store_true', help='classify reads that map to double copy homeologs')
+    parser.add_argument('-t', action='store_true', help='classify reads that map to triple copy homeologs')
+    parser.add_argument('--pe', action='store_true', help='consider paired-end reads together')
+    args = parser.parse_args()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    args = parser.parse_args()
+
+    print('Start:\t{0}'.format(datetime.now()), file=sys.stderr)
+    chrA = {}
+    chrA = readFile(args.A[0], chrA) # file 1
+    chrA = readFile(args.A[1], chrA) # file 2
+    chrA = readFile(args.A[2], chrA) # file 3
+    chrB = {}
+    chrB = readFile(args.B[0], chrB) # file 1
+    chrB = readFile(args.B[1], chrB) # file 2
+    chrB = readFile(args.B[2], chrB) # file 3
+    chrC = {}
+    chrC = readFile(args.C[0], chrC) # file 1
+    chrC = readFile(args.C[1], chrC) # file 2
+    chrC = readFile(args.C[2], chrC) # file 3
+    chrD = {}
+    chrD = readFile(args.D[0], chrD) # file 1
+    chrD = readFile(args.D[1], chrD) # file 2
+    chrD = readFile(args.D[2], chrD) # file 3
+    if args.pe:
+        chrA = combinePE(chrA)
+        chrB = combinePE(chrB)
+        chrC = combinePE(chrC)
+        chrD = combinePE(chrD)
+    writeTable(chrA, chrB, chrC, chrD, args.d, args.u, args.o)
+
+if __name__ == '__main__':
+    main()
+    #os._exit(1)

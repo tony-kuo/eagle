9a0bffd2f19479136afc1d03faaa15597b2d54ba
tony-kuo <tony.cy.kuo@gmail.com>
2021-08-23 10:06:13 -0400

    fixed var_set set creation to remove redundant operations

--- eagle.c
+++ eagle.c
@@ -725,7 +725,8 @@ static void process(const vector_t *var_list, FILE *out_fh) {
     int flag_add = 1;
     while (flag_add) {
         flag_add = 0;
-        for (i = 0; i < var_set->len; i++) {
+        size_t var_set_len = var_set->len
+        for (i = 0; i < var_set_len; i++) {
             vector_t *curr_set = (vector_t *)var_set->data[i];
             if (curr_set->len == 1) continue;
 
